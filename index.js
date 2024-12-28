f6be27af3bf7a8549ae9527746fe263df61d29d8
Rafael Villalobos <rafaelvillalobos_20@hotmail.com>
2024-12-27 22:58:15 -0400

    Timestamp Microservice

--- index.js
+++ index.js
@@ -23,8 +23,22 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
-
+app.get("/api/:date?", function (req, res) {
+  const date = req.params.date;
+  if (!date){
+    return res.json({"unix": new Date().getTime(), "utc": new Date().toUTCString()});
+  } 
+  let parsedDate;
+  if (/^\d+$/.test(date)) {     
+    parsedDate = new Date(Number(date));
+  } else {    
+    parsedDate = new Date(date);
+  }
+  if (isNaN(parsedDate.getTime())) {
+    return res.json({error: "Invalid Date"});
+  }
+  return res.json({"unix": parsedDate.getTime(), "utc": parsedDate.toUTCString()});
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
